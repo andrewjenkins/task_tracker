de3c80cab42b0784a463a542c0735692902d5ce0
Andrew Jenkins <andrewjenkins001@gmail.com>
2017-03-21 11:21:53 -0400

    html outlines and models

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Board(models.Model):
+    owner = models.ForeignKey()
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=64)
+    board = models.ForeignKey(Board)
+
+
+class Item(models.Model):
+    content = models.TextField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = "task_manager/tracker.html"
+    title = "TestEr"
+
+    def get_context_data(self, **kwargs):
+        context = {'title': "tester"}
+        return context
