d4c7aed6fab54e0729c3437fcbf001e3bd455520
Andrew Jenkins <andrewjenkins001@gmail.com>
2017-04-11 01:21:45 -0400

    allow changing groups of items

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.Index.as_view(), name='index'),
     url(r'^group/add$', views.AddGroup.as_view(), name='add_group'),
     url(r'^item/add$', views.AddItem.as_view(), name='add_item'),
+    url(r'^item/move$', views.MoveItem.as_view(), name='move_item'),
     url(r'^items/order', views.OrderItems.as_view(), name="order_items"),
     url(r'^login$', views.Login.as_view(), name='login'),
 ]
