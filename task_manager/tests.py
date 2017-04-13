dd1a1e04106691e82f175faba4c02e74717b4254
Andrew Jenkins <andrewjenkins001@gmail.com>
2017-04-13 15:42:17 -0400

    begin implementing unit tests

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -1,3 +1,23 @@
+from unittest import mock
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Board, Group, Item
 
-# Create your tests here.
+
+class BoardMethodTests(TestCase):
+    def test_has_owner_positive(self):
+        user = User()
+        board = Board(owner=user)
+
+        result = board.has_access(user)
+
+        self.assertTrue(result)
+
+    def test_has_owner_negative(self):
+        user_owner = User()
+        board = Board(owner=user_owner)
+        user_other = User()
+
+        result = board.has_access(user_other)
+
+        self.assertFalse(result)
